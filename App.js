203e0ace4fae1c29c76ee3b04fef9f6bfd87f7cd
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-09-18 21:28:58 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 export default function App() {
+  // JSX = JavaScript XML
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/secure-icon.png')}
+        style={styles.logo}
+      />
+      <View style={styles.sliderArea}>
+         <Slider
+          style={{height: 30}}
+          minimumValue={6}
+          maximumValue={20}
+          thumbTintColor='#3f75fc'
+          maximumTrackTintColor='#c7dceb'
+          minimumTrackTintColor='#33aaff'
+        />
+      </View>
+      <Text>Hello World!</Text>
     </View>
   );
 }
@@ -17,4 +32,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    width: 120,
+    height: 120,
+    marginBottom: 16
+  },
+  sliderArea:{
+    marginTop: 16,
+    marginBottom: 16,
+    padding: 8,
+    borderRadius: 8,
+    width: 200,
+    backgroundColor: "#fff"  
+  }
 });
