45daa736f27dca4624cb437a0f223afc7a5cc7ae
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-10-16 20:57:10 -0300

    Aula 16/10

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Slider from '@react-native-community/slider';
+import { useState } from 'react';
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!*&-_$"
 
 export default function App() {
+
+  const [size, setSize] = useState(10)
+  const [password, setPassword] = useState("")
+
+  function generatePassword(){
+    let value = ""
+    for(let i = 0; i < size; i++){
+        let randomIndex = Math.floor(Math.random() * charset.length)
+        value += charset.charAt(randomIndex)
+    }
+    console.log("Senha: " + value)
+    setPassword(value)
+  }
+
+
   // JSX = JavaScript XML
   return (
     <View style={styles.container}>
@@ -10,7 +28,8 @@ export default function App() {
         source={require('./assets/secure-icon.png')}
         style={styles.logo}
       />
-      <Text style={styles.title}>10 Caracteres</Text>
+      <Text style={styles.title}>{size} Caracteres</Text>
+      <Text style={styles.textPassword}>Senha Gerada: {password}</Text>
       <View style={styles.sliderArea}>
          <Slider
           style={{height: 30}}
@@ -19,12 +38,16 @@ export default function App() {
           thumbTintColor='#3f75fc'
           maximumTrackTintColor='#c7dceb'
           minimumTrackTintColor='#33aaff'
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
         />
       </View>
       <Pressable style={({pressed}) => [
         styles.button,
         {opacity: (pressed) ? 0.7 : 1}
-      ]}>
+      ]}
+        onPress={generatePassword}
+      >
         <Text style={styles.buttonText} selectable={false}>Gerar Senha</Text>
       </Pressable>
     </View>
@@ -67,5 +90,10 @@ const styles = StyleSheet.create({
   buttonText:{
     color: '#fff',
     fontSize: 20
+  },
+  textPassword:{
+    fontSize: 20,
+    color: '#274c6aff',
+    fontWeight: 'bold'
   }
 });
